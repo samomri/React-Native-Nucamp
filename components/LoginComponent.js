945ae35e7c68b49082420d69d45700c05348b74d
samomri <samquantadev@gmail.com>
2022-02-12 04:35:02 -0500

    lastCommit

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,8 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
+import { manipulateAsync, SaveFormat } from 'expo-image-manipulator'
+import { saveToLibraryAsync } from 'expo-media-library'//Bonus
 
 class LoginTab extends Component {
 
@@ -159,9 +161,30 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri});
+                saveToLibraryAsync(capturedImage.uri); //Bonus
+                this.processImage(capturedImage.uri);
             }
         }
+    }
+
+    getImageFromGallery = async () => {
+        const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermission.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            console.log(capturedImage);
+            this.processImage(capturedImage.uri);
+        }
+    }
+
+    processImage = async (imgUri) => {
+        const processedImage = await manipulateAsync(imgUri,[{resize: {width : 400} }],SaveFormat.PNG);
+        console.log(processedImage);
+        this.setState({imageUrl: processedImage.uri});
+
     }
 
     handleRegister() {
@@ -190,6 +213,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input
